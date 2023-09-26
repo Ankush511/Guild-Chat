c84e45ca1f7a0746205189007059f696d23eba2b
Ankush Kashyap <511ankush007@gmail.com>
2023-09-26 17:24:05 +0530

    managing members done

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 
 
 export const ModalProvider = () => {
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   )
 }
