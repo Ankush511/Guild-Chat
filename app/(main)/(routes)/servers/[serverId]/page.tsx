e32f73cb7a731a4a5b761c1a313ed5e2913a7fb6
Ankush Kashyap <511ankush007@gmail.com>
2023-10-09 22:44:19 +0530

    channel id page done

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,52 @@
-const ServerIdPage = () => {
-  return ( 
-    <div>This Is Server Id Page!</div>
-   );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  }
+};
+
+const ServerIdPage = async ({
+  params,
+}: ServerIdPageProps) => {
+
+  const profile = await currentProfile();
+
+  if(!profile){
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }
+    }
+  });
+
+  const inititalChannel = server?.channels[0];
+
+  if(inititalChannel?.name !== "general"){
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${inititalChannel?.id}`);
 }
- 
+
 export default ServerIdPage;
