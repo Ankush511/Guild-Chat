7b020603c479583a3590fed00dc7bf742de2a4b5
Ankush Kashyap <511ankush007@gmail.com>
2023-10-18 22:35:14 +0530

    attachment done

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io",
+      
+    ]
+  }
+}
+
+module.exports = nextConfig
