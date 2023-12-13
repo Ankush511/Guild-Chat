f4d5e7114d5c47ef9f20e2a68eb5a3d9b92c16dc
Ankush Kashyap <511ankush007@gmail.com>
2023-12-13 15:41:34 +0530

    final

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
         inviteCode: uuidv4(),
         channels: {
           create: [
-            { name: "general", profileID: profile.id }
+            { name: "general", profileId: profile.id }
           ]
         },
         members: {
